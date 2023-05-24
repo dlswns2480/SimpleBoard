4da23b406313233cdb9bb552455ff28e67b021f0
dlswns2480 <dlswns2480@naver.com>
2023-05-24 22:31:25 +0900

    게시글 목록의 상세페이지까지의 구현

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,40 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriteFrom(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+        boardService.write(board);
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList()); //board에 있는 데이터들을 "list"라는 이름에 담아서 보내겠다.
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardview";
+    }
+}
