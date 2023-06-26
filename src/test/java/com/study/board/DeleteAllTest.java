2f76bb88f623ee107a158910eca61938681e2df6
dlswns2480 <dlswns2480@naver.com>
2023-06-26 16:05:28 +0900

    리스트 화면에서 글 생성 전체삭제 기능 추가

--- src/test/java/com/study/board/DeleteAllTest.java
+++ src/test/java/com/study/board/DeleteAllTest.java
@@ -1,4 +1,16 @@
 package com.study.board;
 
+import com.study.board.service.BoardService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class DeleteAllTest {
+    @Autowired
+    private BoardService boardservice;
+
+    @Test
+    public void deleteallTest(){
+        boardservice.boardDelete(4);
+    }
 }
